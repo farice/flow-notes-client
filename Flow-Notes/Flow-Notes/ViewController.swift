6daff84cadd19e1bcfb59f96921a26af60dc94d5
farice <farissbahi@gmail.com>
2017-12-25 13:06:59 -0500

    Add Apollo
    Useful cocoapod for interacting with our graphql backend

---
+++ Flow-Notes/Flow-Notes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flow-Notes
+//
+//  Created by Faris Sbahi on 12/25/17.
+//  Copyright © 2017 Pauca, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
